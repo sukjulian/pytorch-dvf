80bd915d45bf5702796f981413424eb507a4ae0f
j.m.suk@utwente.nl <sukjm@hpc-node02.local>
2024-06-20 11:18:55 +0200

    version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="torch_dvf",
-    version="0.1.0",
+    version="0.1.1",
     author="Julian Suk",
     author_email="j.m.suk@utwente.nl",
     license="MIT",
