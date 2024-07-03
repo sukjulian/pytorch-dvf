3bf73ab2bc12d752dbcd83f7cb311fe7807d1782
Patryk Rygiel <p.t.rygiel@utwente.nl>
2024-07-03 11:29:51 +0200

    Remove: geodesic heat method

--- torch_dvf/transforms.py
+++ torch_dvf/transforms.py
@@ -1,7 +1,6 @@
 from typing import Optional
 
 import networkx as nx
-import potpourri3d as pp3d
 import torch
 import torch_geometric as pyg
 
@@ -234,97 +233,6 @@ class SkeletonPointCloudHierarchy:
             pos = pos[sampling_idcs]
             batch = batch[sampling_idcs]
             skeleton_map = skeleton_map[sampling_idcs]
-
-        return Data(**data)
-
-    def __repr__(self) -> str:
-
-        repr_str = (
-            "{}(rel_sampling_ratios={}, cluster_dists={}, interp_simplex={})".format(
-                self.__class__.__name__,
-                self.rel_sampling_ratios,
-                self.cluster_dists,
-                self.interp_simplex,
-            )
-        )
-
-        return repr_str
-
-
-class GeodesicPointCloudHierarchy:
-    """Nested hierarchy of sub-sampled point clouds. Graph edges connect each coarse-scale point to a cluster of fine-scale points.
-    Barycentric interpolation from the coarse to the fine scales. For correct batching, "torch_geometric.data.Data.__inc__()" has to be
-    overridden.
-
-    Points are clustered according to induced manifold.
-
-    Args:
-        rel_sampling_ratios (tuple): relative ratios for successive farthest point sampling
-        cluster_dists (tuple): geodesic distances for clusters
-        interp_simplex (str): reference simplex for barycentric interpolation ('triangle' or 'tetrahedron')
-    """
-
-    def __init__(
-        self, rel_sampling_ratios: tuple, cluster_dists: tuple, interp_simplex: str
-    ):
-        self.rel_sampling_ratios = rel_sampling_ratios
-        self.cluster_dists = cluster_dists
-        self.interp_simplex = interp_simplex
-
-        self.dim_interp_simplex = {"triangle": 2, "tetrahedron": 3}[interp_simplex]
-
-    def _get_pooling_scale(
-        self, pos: torch.tensor, sampling_idcs: torch.tensor, cluster_dist: float
-    ):
-        solver = pp3d.PointCloudHeatSolver(pos.numpy())
-
-        pool_edge_index = []
-        for i, sampling_idc in enumerate(sampling_idcs):
-            dists = torch.tensor(solver.compute_distance(sampling_idc))
-            pool_source = torch.argwhere(dists <= cluster_dist).squeeze()
-            pool_target = torch.ones_like(pool_source) * i
-            pool_edge_index.append(torch.stack([pool_target, pool_source]))
-
-        return torch.cat(pool_edge_index, -1)
-
-    def __call__(self, data: pyg.data.Data) -> Data:
-
-        pos = data.pos
-        batch = (
-            data.surface_id.long()
-            if hasattr(data, "surface_id")
-            else torch.zeros(pos.size(0), dtype=torch.long)
-        )
-
-        for i, (sampling_ratio, cluster_dist) in enumerate(
-            zip(self.rel_sampling_ratios, self.cluster_dists)
-        ):
-
-            sampling_idcs = fps(pos, batch, ratio=sampling_ratio)
-
-            pool_target, pool_source = self._get_pooling_scale(
-                pos, sampling_idcs, cluster_dist
-            )
-            interp_target, interp_source = knn(
-                pos[sampling_idcs],
-                pos,
-                self.dim_interp_simplex + 1,
-                batch[sampling_idcs],
-                batch,
-            )
-
-            data[f"scale{i}_pool_target"], data[f"scale{i}_pool_source"] = (
-                pool_target.int(),
-                pool_source.int(),
-            )
-            data[f"scale{i}_interp_target"], data[f"scale{i}_interp_source"] = (
-                interp_target.int(),
-                interp_source.int(),
-            )
-            data[f"scale{i}_sampling_index"] = sampling_idcs.int()
-
-            pos = pos[sampling_idcs]
-            batch = batch[sampling_idcs]
 
         return Data(**data)
 
