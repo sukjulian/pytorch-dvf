8ae80e65ee059f59c5ad88b7f72205f7b4d12818
Patryk Rygiel <p.t.rygiel@utwente.nl>
2024-07-04 16:54:38 +0200

    Add: abstract class for point cloud hierarchies

--- torch_dvf/transforms.py
+++ torch_dvf/transforms.py
@@ -1,4 +1,11 @@
-from typing import Optional, Tuple
+from abc import ABC, abstractmethod
+from typing import (
+    Dict,
+    List,
+    Optional,
+    Tuple,
+    Union,
+)
 
 import networkx as nx
 import torch
@@ -9,9 +16,85 @@ from torch_cluster import fps, knn, radius
 from .data import Data
 
 
-class RadiusPointCloudHierarchy:
-    """Nested hierarchy of sub-sampled point clouds. Graph edges connect each coarse-scale point to a cluster of fine-scale points.
+class PointCloudHierarchy(ABC):
+    """Abstract class for nested hierarchy of sub-sampled point clouds. Graph edges connect each coarse-scale point to a cluster of fine-scale points.
     Interpolation from the coarse to the fine scales. For correct batching, "torch_geometric.data.Data.__inc__()" has to be overridden.
+
+    Args:
+        rel_sampling_ratios (Tuple[float]): relative ratios for successive farthest point sampling
+        interp_simplex (str): reference simplex for interpolation ('triangle' or 'tetrahedron')
+    """
+
+    def __init__(self, rel_sampling_ratios: Tuple[float], interp_simplex: str):
+        self.rel_sampling_ratios = rel_sampling_ratios
+        self.interp_simplex = interp_simplex
+        self.dim_interp_simplex = {"triangle": 2, "tetrahedron": 3}[interp_simplex]
+        self.subsample_keys = ["pos", "batch"]
+
+    def _init_state(self, data: pyg.data.Data) -> Dict[str, torch.Tensor]:
+        pos = data.pos
+        batch = (
+            data.surface_id.long()
+            if hasattr(data, "surface_id")
+            else torch.zeros(pos.size(0), dtype=torch.long)
+        )
+
+        return {"pos": pos, "batch": batch}
+
+    def _update_state(
+        self, state: Dict[str, torch.Tensor], sampling_idcs: torch.Tensor
+    ) -> Dict[str, torch.Tensor]:
+
+        for key in self.subsample_keys:
+            state[key] = state[key][sampling_idcs]
+        return state
+
+    @abstractmethod
+    def _get_hierarchy(
+        self, scale_id: int, sampling_idcs: torch.Tensor, **kwargs
+    ) -> Union[torch.Tensor]:
+        pass
+
+    def __call__(self, data: pyg.data.Data) -> Data:
+        state_dict = self._init_state(data)
+
+        for scale_id, rel_sampling_ratio in enumerate(self.rel_sampling_ratios):
+            pos, batch = state_dict["pos"], state_dict["batch"]
+            sampling_idcs = fps(pos, batch, ratio=rel_sampling_ratio)
+
+            pool_target, pool_source = self._get_hierarchy(
+                scale_id, sampling_idcs, **state_dict
+            )
+            interp_target, interp_source = knn(
+                pos[sampling_idcs],
+                pos,
+                self.dim_interp_simplex + 1,
+                batch[sampling_idcs],
+                batch,
+            )
+
+            (
+                data[f"scale{scale_id}_pool_target"],
+                data[f"scale{scale_id}_pool_source"],
+            ) = (
+                pool_target.int(),
+                pool_source.int(),
+            )
+            (
+                data[f"scale{scale_id}_interp_target"],
+                data[f"scale{scale_id}_interp_source"],
+            ) = (
+                interp_target.int(),
+                interp_source.int(),
+            )
+            data[f"scale{scale_id}_sampling_index"] = sampling_idcs.int()
+            state_dict = self._update_state(state_dict, sampling_idcs)
+
+        return Data(**data)
+
+
+class RadiusPointCloudHierarchy(PointCloudHierarchy):
+    """Clustering point cloud according to ball radius.
 
     Args:
         rel_sampling_ratios (Tuple[float]): relative ratios for successive farthest point sampling
@@ -26,8 +109,7 @@ class RadiusPointCloudHierarchy:
         cluster_radii: Optional[Tuple[float]] = None,
         max_num_neighbors: int = 32,
     ):
-        self.rel_sampling_ratios = rel_sampling_ratios
-        self.interp_simplex = interp_simplex
+        super().__init__(rel_sampling_ratios, interp_simplex)
         self.max_num_neighbors = max_num_neighbors
 
         self.cluster_radii_arg = cluster_radii  # for "__repr__()"
@@ -35,61 +117,35 @@ class RadiusPointCloudHierarchy:
             cluster_radii if cluster_radii else [None] * len(rel_sampling_ratios)
         )
 
-        self.dim_interp_simplex = {"triangle": 2, "tetrahedron": 3}[interp_simplex]
+    def _get_hierarchy(
+        self,
+        scale_id: int,
+        sampling_idcs: torch.Tensor,
+        pos: torch.tensor,
+        batch: torch.Tensor,
+        **kwargs,
+    ) -> Union[torch.Tensor]:
 
-    def __call__(self, data: pyg.data.Data) -> Data:
-        pos = data.pos
-        batch = (
-            data.surface_id.long()
-            if hasattr(data, "surface_id")
-            else torch.zeros(pos.size(0), dtype=torch.long)
+        cluster_radius = self.cluster_radii[scale_id]
+
+        if cluster_radius is None:
+            cluster_radius = self._estimate_cluster_radius(
+                pos, pos[sampling_idcs], batch, batch[sampling_idcs]
+            )
+            self.cluster_radii[scale_id] = cluster_radius
+
+        pool_target, pool_source = radius(
+            pos,
+            pos[sampling_idcs],
+            cluster_radius,
+            batch,
+            batch[sampling_idcs],
+            max_num_neighbors=self.max_num_neighbors,
         )
 
-        for i, (sampling_ratio, cluster_radius) in enumerate(
-            zip(self.rel_sampling_ratios, self.cluster_radii)
-        ):
-            sampling_idcs = fps(
-                pos, batch, ratio=sampling_ratio
-            )  # takes some time but is worth it
+        return pool_target, pool_source
 
-            if cluster_radius is None:
-                cluster_radius = self.estimate_cluster_radius(
-                    pos, pos[sampling_idcs], batch, batch[sampling_idcs]
-                )
-                self.cluster_radii[i] = cluster_radius
-
-            pool_target, pool_source = radius(
-                pos,
-                pos[sampling_idcs],
-                cluster_radius,
-                batch,
-                batch[sampling_idcs],
-                max_num_neighbors=self.max_num_neighbors,
-            )
-            interp_target, interp_source = knn(
-                pos[sampling_idcs],
-                pos,
-                self.dim_interp_simplex + 1,
-                batch[sampling_idcs],
-                batch,
-            )
-
-            data[f"scale{i}_pool_target"], data[f"scale{i}_pool_source"] = (
-                pool_target.int(),
-                pool_source.int(),
-            )
-            data[f"scale{i}_interp_target"], data[f"scale{i}_interp_source"] = (
-                interp_target.int(),
-                interp_source.int(),
-            )
-            data[f"scale{i}_sampling_index"] = sampling_idcs.int()
-
-            pos = pos[sampling_idcs]
-            batch = batch[sampling_idcs]
-
-        return Data(**data)
-
-    def estimate_cluster_radius(
+    def _estimate_cluster_radius(
         self, pos_source, pos_target, batch_source, batch_target
     ) -> float:
         k = {"triangle": 7, "tetrahedron": 14}[self.interp_simplex]
@@ -116,12 +172,8 @@ class RadiusPointCloudHierarchy:
         return repr_str
 
 
-class SkeletonPointCloudHierarchy:
-    """Nested hierarchy of sub-sampled point clouds. Graph edges connect each coarse-scale point to a cluster of fine-scale points.
-    Barycentric interpolation from the coarse to the fine scales. For correct batching, "torch_geometric.data.Data.__inc__()" has to be
-    overridden.
-
-    Points are clustered based on the provided unerlying skeleton graph according to:
+class SkeletonPointCloudHierarchy(PointCloudHierarchy):
+    """Clustering point cloud according to underlying skeleton graph, based on:
     https://link.springer.com/chapter/10.1007/978-3-031-43990-2_73
 
     Args:
@@ -137,12 +189,11 @@ class SkeletonPointCloudHierarchy:
         interp_simplex: str,
         max_num_neighbors: Optional[int] = None,
     ):
-        self.rel_sampling_ratios = rel_sampling_ratios
-        self.cluster_dists = cluster_dists
-        self.interp_simplex = interp_simplex
-        self.max_num_neighbors = max_num_neighbors
+        super().__init__(rel_sampling_ratios, interp_simplex)
 
-        self.dim_interp_simplex = {"triangle": 2, "tetrahedron": 3}[interp_simplex]
+        self.cluster_dists = cluster_dists
+        self.max_num_neighbors = max_num_neighbors
+        self.subsample_keys += ["skeleton_map"]
 
     def _construct_nx_graph(
         self, centerline_edge_index: torch.tensor, edge_lengths: torch.tensor
@@ -166,6 +217,8 @@ class SkeletonPointCloudHierarchy:
         skeleton_pos: torch.tensor,
         skeleton_edge_index: torch.tensor,
     ):
+        """Precomputing skeleton graph all-pair distance lookup table (LUT)"""
+
         # Compute mapping from surface onto skeleton
         _, skeleton_map = knn(skeleton_pos, pos, 1)
 
@@ -179,14 +232,30 @@ class SkeletonPointCloudHierarchy:
 
         return dist_LUT, skeleton_map
 
-    def _get_pooling_scale(
+    def _init_state(
+        self, data: pyg.data.Data
+    ) -> Union[Dict[str, torch.Tensor], List[str]]:
+        state = super()._init_state(data)
+        dist_LUT, skeleton_map = self._skeleton_distance_LUT(
+            data.pos, data.skeleton_pos, data.skeleton_edge_index
+        )
+
+        state["dist_LUT"] = dist_LUT
+        state["skeleton_map"] = skeleton_map
+
+        return state
+
+    def _get_hierarchy(
         self,
-        dist_LUT: torch.tensor,
-        skeleton_map: torch.tensor,
-        sampling_idcs: torch.tensor,
-        cluster_dist: float,
-        batch: torch.tensor,
-    ):
+        scale_id: int,
+        sampling_idcs: torch.Tensor,
+        batch: torch.Tensor,
+        dist_LUT: torch.Tensor,
+        skeleton_map: torch.Tensor,
+        **kwargs,
+    ) -> Union[torch.Tensor]:
+
+        cluster_dist = self.cluster_dists[scale_id]
         skeleton_adj = dist_LUT <= cluster_dist
 
         pool_edge_index = []
@@ -210,52 +279,6 @@ class SkeletonPointCloudHierarchy:
             pool_edge_index.append(torch.stack([pool_target, pool_source]))
 
         return torch.cat(pool_edge_index, -1)
-
-    def __call__(self, data: pyg.data.Data) -> Data:
-        pos = data.pos
-        skeleton_pos = data.skeleton_pos
-        skeleton_edge_index = data.skeleton_edge_index
-
-        dist_LUT, skeleton_map = self._skeleton_distance_LUT(
-            pos, skeleton_pos, skeleton_edge_index
-        )
-        batch = (
-            data.surface_id.long()
-            if hasattr(data, "surface_id")
-            else torch.zeros(pos.size(0), dtype=torch.long)
-        )
-
-        for i, (sampling_ratio, cluster_dist) in enumerate(
-            zip(self.rel_sampling_ratios, self.cluster_dists)
-        ):
-            sampling_idcs = fps(pos, batch, ratio=sampling_ratio)
-
-            pool_target, pool_source = self._get_pooling_scale(
-                dist_LUT, skeleton_map, sampling_idcs, cluster_dist, batch
-            )
-            interp_target, interp_source = knn(
-                pos[sampling_idcs],
-                pos,
-                self.dim_interp_simplex + 1,
-                batch[sampling_idcs],
-                batch,
-            )
-
-            data[f"scale{i}_pool_target"], data[f"scale{i}_pool_source"] = (
-                pool_target.int(),
-                pool_source.int(),
-            )
-            data[f"scale{i}_interp_target"], data[f"scale{i}_interp_source"] = (
-                interp_target.int(),
-                interp_source.int(),
-            )
-            data[f"scale{i}_sampling_index"] = sampling_idcs.int()
-
-            pos = pos[sampling_idcs]
-            batch = batch[sampling_idcs]
-            skeleton_map = skeleton_map[sampling_idcs]
-
-        return Data(**data)
 
     def __repr__(self) -> str:
         repr_str = (
